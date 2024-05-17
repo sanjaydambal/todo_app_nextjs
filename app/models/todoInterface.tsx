8c0871964a0727c46f5e54a918c9b3994f135406
sanjaydambal <sanjaydambal@gmail.com>
2024-05-17 17:31:54 +0530

    created todo component

---
+++ app/models/todoInterface.tsx
@@ -0,0 +1,13 @@
+export interface TodoInterface {
+    id: number;
+    title: string;
+    description: string;
+    completed: boolean;
+  }
+  
+  export interface AddTodoInterface {
+    title: string;
+    description: string;
+    completed: boolean;
+  }
+  
